ff39154b52bf6fd603bd03b70a67b52318d29c09
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-29 01:43:14 +0900

    Feat: Console로 map 입력 받는 기능 구현

--- src/main/java/sokoban/view/InputView.java
+++ src/main/java/sokoban/view/InputView.java
@@ -1,8 +1,11 @@
 package sokoban.view;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class InputView {
+    private final String SEPARATOR = "=====";
     private Scanner scanner;
 
     public int readStage() {
@@ -10,5 +13,18 @@ public class InputView {
         String input = scanner.nextLine();
         String[] split = input.trim().split(" ");
         return Integer.parseInt(split[1]);
+    }
+
+    public Map<Integer, String> readMap() {
+        scanner = new Scanner(System.in);
+        Map<Integer, String> map = new HashMap<>();
+        int key = 0;
+        String input = "";
+        while (!input.equals(SEPARATOR)) {
+            input = scanner.nextLine();
+            map.put(key, input);
+            key++;
+        }
+        return map;
     }
 }
