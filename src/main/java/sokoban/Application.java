6964d76b05c02deb807e01ab6514854c275ce15f
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-29 12:20:16 +0900

    Feat: 입력값 없을 때까지 console 읽고 GameBoard 객체 생성

--- src/main/java/sokoban/Application.java
+++ src/main/java/sokoban/Application.java
@@ -1,6 +1,22 @@
 package sokoban;
 
+import sokoban.domain.GameBoard;
+import sokoban.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InputView inputView = new InputView(scanner);
+        List<GameBoard> gameBoards = new ArrayList<>();
+        while (scanner.hasNext()) {
+            int stage = inputView.readStage();
+            Map<Integer, String> map = inputView.readMap();
+            gameBoards.add(new GameBoard(stage, map));
+        }
     }
 }
--- src/main/java/sokoban/view/InputView.java
+++ src/main/java/sokoban/view/InputView.java
@@ -9,15 +9,17 @@ import java.util.Scanner;
 public class InputView {
     private Scanner scanner;
 
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     public int readStage() {
-        scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String[] split = input.trim().split(" ");
         return Integer.parseInt(split[1]);
     }
 
     public Map<Integer, String> readMap() {
-        scanner = new Scanner(System.in);
         Map<Integer, String> map = new HashMap<>();
         int key = 0;
         String input = "";
