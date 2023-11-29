3905ff102c3bbc4c8c4ccdeee94fd514bfb0bccd
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-29 12:35:30 +0900

    Feat: Map 정보 출력하는 코드 작성

--- src/main/java/sokoban/Application.java
+++ src/main/java/sokoban/Application.java
@@ -2,6 +2,7 @@ package sokoban;
 
 import sokoban.domain.GameBoard;
 import sokoban.view.InputView;
+import sokoban.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,15 @@ public class Application {
             int stage = inputView.readStage();
             Map<Integer, String> map = inputView.readMap();
             gameBoards.add(new GameBoard(stage, map));
+        }
+
+        OutputView outputView = new OutputView();
+        for (GameBoard game : gameBoards) {
+            int stage = game.getStage();
+            Map<Integer, List<Integer>> map = game.getMap();
+            outputView.printMap(stage, map);
+            outputView.printMapInfo(game.getWidth(), game.getLength(), game.findHalls(), game.findBalls(), game.findPlayerPosition());
+            System.out.println();
         }
     }
 }
